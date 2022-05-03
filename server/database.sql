2eb78b00a137432cc1af38ea9a4b058ec1bf6c3e
Sitesh P <siteshpattanaik001@gmail.com>
2022-05-03 01:14:31 -0700

    redesigned student-helper

---
+++ server/database.sql
@@ -0,0 +1,6 @@
+CREATE DATABASE student_helper;
+
+CREATE TABLE todo(
+  todo_id SERIAL PRIMARY KEY,
+  description VARCHAR(255)
+);
---
+++ server/db.js
@@ -0,0 +1,11 @@
+const Pool = require("pg").Pool;
+
+const pool = new Pool({
+  user: "",
+  password: "",
+  host: "localhost",
+  port: 5432,
+  database: "student_helper"
+});
+
+module.exports = pool;
