a7efa24cf02e154274123bc0ab212fbdc865d038
Sitesh P <siteshpattanaik001@gmail.com>
2022-05-10 15:08:20 -0700

    kanban-ui fix mostly

--- client/src/components/StoryCard.js
+++ client/src/components/StoryCard.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, Grid, Card, CardGroup } from "semantic-ui-react";
+import { Container, Grid, Card, CardGroup, Icon } from "semantic-ui-react";
 import "../App.css";
 import Footer from "./Footer";
 import MenuHeader from "./MenuHeader";
@@ -15,8 +15,23 @@ const StoryCard = (props) => {
           style={props.text_style}
         />
       </Card.Content>
+      <Card.Content extra >
+      <a style={props.text_style}>
+        <Icon name='user' />
+        Developer
+      </a>
+      <a style={props.text_style, {position:'absolute', right:'55px', color:'white', border: '1px dashed black', padding:'3px', borderRadius:'5px'}}>
+        TS-123
+      </a>
+      
+      <a style={props.text_style, {position:'absolute', right:'10px', color:'white', border: '1px dashed black', padding:'3px', borderRadius:'5px'}}>
+        <Icon name='clock outline' />
+        2
+      </a>
+    </Card.Content>
     </Card>
   );
 };
+
 
 export default StoryCard;
