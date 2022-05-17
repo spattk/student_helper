f9176f6517ed35251c6cb41feb4c95d5596bb3f6
Sitesh P <siteshpattanaik001@gmail.com>
2022-05-17 15:03:47 -0700

    story update API works

--- client/src/components/Kanban.js
+++ client/src/components/Kanban.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { Card, Container, Grid } from "semantic-ui-react";
+import { Button, Card, Container, Grid } from "semantic-ui-react";
 import "../App.css";
 import Footer from "./Footer";
 import MenuHeader from "./MenuHeader";
@@ -69,6 +69,35 @@ const Kanban = (props) => {
     getProjectDetails();
   }, [id]);
 
+  const testMeClickHandler = () => {
+    console.log("hey there");
+    let newArray = projectStories;
+    for(var i=0;i<newArray.length;i++){
+      if(newArray[i].status === "TODO"){
+        newArray[i].status = "IN_PROGRESS";
+      }
+    }
+    console.log(newArray);
+    setProjectStories(newArray);
+
+    let temp = [];
+    setProjectStories((state) => {
+      temp = state.filter((story) => story.status === "TODO");
+      setTodoStories(temp);
+
+      temp = state.filter((story) => story.status === "IN_PROGRESS");
+      setDevStories(temp);
+
+      temp = state.filter((story) => story.status === "IN_REVIEW");
+      setReviewStories(temp);
+
+      temp = state.filter((story) => story.status === "COMPLETED");
+      setReleasedStories(temp);
+
+      return state;
+    });
+  } 
+
   return (
     <Container fluid={true}>
       <MenuHeader />
@@ -97,12 +126,13 @@ const Kanban = (props) => {
                   >
                     {todoStories.map((story, index) => (
                       <StoryCard
-                        key={index}
+                        key={story.story_id}
                         content={story.story_description}
                         card_style={styles[0]}
                         developer_name = {story.developer_id}
                         story_points = {story.story_points}
                         story_id = {story.story_id}
+                        update_story_handler = {getProjectDetails}
                       />
                     ))}
                     {/* <StoryCard
@@ -132,18 +162,19 @@ const Kanban = (props) => {
                   </Card.Group>
                 </Grid.Column>
                 <Grid.Column width={4}>
-                  <h4 style={{ textAlign: "center" }}>DEV</h4>
+                  <h4 style={{ textAlign: "center" }}>IN_PROGRESS</h4>
                   <Card.Group
                     style={{ border: "1px dashed grey", borderRadius: "10px" }}
                   >
                     {devStories.map((story, index) => (
                       <StoryCard
-                        key={index}
+                        key={story.story_id}
                         content={story.story_description}
                         card_style={styles[1]}
                         developer_name = {story.developer_id}
                         story_points = {story.story_points}
                         story_id = {story.story_id}
+                        update_story_handler = {getProjectDetails}
                       />
                     ))}
 
@@ -164,19 +195,20 @@ const Kanban = (props) => {
                   </Card.Group>
                 </Grid.Column>
                 <Grid.Column width={4}>
-                  <h4 style={{ textAlign: "center" }}>QA/REVIEW</h4>
+                  <h4 style={{ textAlign: "center" }}>IN_REVIEW</h4>
                   <Card.Group
                     style={{ border: "1px dashed grey", borderRadius: "10px" }}
                   >
                     {reviewStories.map((story, index) => (
                       <StoryCard
-                        key={index}
+                        key={story.story_id}
                         content={story.story_description}
                         card_style={styles[2]}
                         text_style={{ color: "white" }}
                         developer_name = {story.developer_id}
                         story_points = {story.story_points}
                         story_id = {story.story_id}
+                        update_story_handler = {getProjectDetails}
                       />
                     ))}
 
@@ -200,19 +232,20 @@ const Kanban = (props) => {
                   </Card.Group>
                 </Grid.Column>
                 <Grid.Column width={4}>
-                  <h4 style={{ textAlign: "center" }}>RELEASED</h4>
+                  <h4 style={{ textAlign: "center" }}>COMPLETED</h4>
                   <Card.Group
                     style={{ border: "1px dashed grey", borderRadius: "10px" }}
                   >
                     {releasedStories.map((story, index) => (
                       <StoryCard
-                        key={index}
+                        key={story.story_id}
                         content={story.story_description}
                         card_style={styles[3]}
                         text_style={{ color: "white" }}
                         developer_name = {story.developer_id}
                         story_points = {story.story_points}
                         story_id = {story.story_id}
+                        update_story_handler = {getProjectDetails}
                       />
                     ))}
 
--- client/src/components/StoryCard.js
+++ client/src/components/StoryCard.js
@@ -1,18 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
 import { Card, Icon } from "semantic-ui-react";
 import "../App.css";
 
 const StoryCard = (props) => {
+  const [addrtype, setAddrtype] = useState([
+    "<status>",
+    "TODO",
+    "IN_PROGRESS",
+    "IN_REVIEW",
+    "COMPLETED",
+  ]);
+  const Add = addrtype.map((Add) => Add);
+  const handleAddrTypeChange = (e) => {
+    let storyId = e.target.parentElement.id;
+    let updatedStoryStatus = addrtype[e.target.value];
+    console.log(addrtype[e.target.value] + " " + storyId);
+    console.log(updatedStoryStatus);
+    const requestOptions = {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        story_name: "Create Story API",
+        story_description: "Kanban board story create API",
+        story_points: "2",
+        status: updatedStoryStatus,
+      }),
+    };
+    fetch(`http://localhost:5001/stories/${storyId}`, requestOptions).then(
+      (response) =>
+        response.json().then((result) => {
+          console.log(result);
+          props.update_story_handler();
+        })
+    );
+  };
+
   return (
     <Card style={props.card_style}>
       <Card.Content>
         <Card.Description content={props.content} style={props.text_style} />
+        <br />
+        <div
+          style={{ width: "50%", float: "right", fontSize: "13px" }}
+          id={props.story_id}
+        >
+          <select
+            onChange={(e) => handleAddrTypeChange(e)}
+            className="custom-select"
+          >
+            {Add.map((address, key) => (
+              <option key={key} value={key}>
+                {address}
+              </option>
+            ))}
+          </select>
+        </div>
       </Card.Content>
+
       <Card.Content extra>
         <a style={props.text_style}>
           <Icon name="user" />
           {props.developer_name}
         </a>
+
         <a
           style={
             (props.text_style,
@@ -26,7 +76,7 @@ const StoryCard = (props) => {
             })
           }
         >
-          TS-{props.story_id}
+          KB-{props.story_id}
         </a>
 
         <a
@@ -42,7 +92,8 @@ const StoryCard = (props) => {
             })
           }
         >
-          <Icon name="clock outline" />{props.story_points}
+          <Icon name="clock outline" />
+          {props.story_points}
         </a>
       </Card.Content>
     </Card>
