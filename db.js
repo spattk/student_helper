719bad4dca190acd5f9d300932a506df13ee63e5
Sitesh P <siteshpattanaik001@gmail.com>
2022-06-03 23:49:04 -0700

    db.js fix

--- db.js
+++ db.js
@@ -1,18 +1,26 @@
 const Pool = require("pg").Pool;
-require("dotenv").config()
+require("dotenv").config();
 
 const devConfig = {
   user: process.env.PG_USER,
   password: process.env.PG_PASSWORD,
   host: process.env.PG_HOST,
   port: process.env.PG_PORT,
-  database: process.env.PG_DATABASE
-}
+  database: process.env.PG_DATABASE,
+};
 
 const proConfig = {
-  connectionString: process.env.DATABASE_URL //heroku addons
-}
+  connectionString: process.env.DATABASE_URL, //heroku addons
+};
 
-const pool = new Pool(process.env.NODE_ENV === "production" ? proConfig : devConfig);
+const pool =
+  process.env.NODE_ENV === "production"
+    ? new Pool({
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      })
+    : new Pool(devConfig);
 
 module.exports = pool;
